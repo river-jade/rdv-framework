67b817c482627e9b623c2ecc91170b7e6a705f3b
burn2shine <burn2shine@0edbd1c7-074e-7333-919a-b20e77d7077a>
2011-09-22 04:25:30 +0000

    Add example of NormalDitributionGenerator to example project.

--- trunk/R/example.R
+++ trunk/R/example.R
@@ -13,6 +13,7 @@ for (i in 1:20) {
 cat('Value for test variable 1 is:', test.variable.1, '\n')
 cat('Value for test variable 2 is:', test.variable.2, '\n')
 cat('Value for test variable 3 is:', test.variable.3, '\n')
+cat('Value for test variable 4 is:', test.variable.4, '\n')
 
 cat('The working dir is', getwd(), '\n')
 cat('PAR.testing.output.filename=', PAR.testing.output.filename, '\n')
