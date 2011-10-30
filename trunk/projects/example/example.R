48d5f944c2da15e942304e613609f75a594a380b
ascelin <ascelin@0edbd1c7-074e-7333-919a-b20e77d7077a>
2011-10-30 11:59:51 +0000

    minor changes to CPW-simple and example project

--- trunk/projects/example/example.R
+++ trunk/projects/example/example.R
@@ -8,6 +8,10 @@ for (i in 1:20) {
   Sys.sleep(.005)
 } 
 
+x <- matrix( nrow=20, ncol=20)
+x[] <- 1:(20*20) 
+
+
 cat('Value for test variable 1 is:', tzar[['test.variable.1']], '\n')
 cat('Value for test variable 2 is:', tzar[['test.variable.2']], '\n')
 cat('Value for test variable 3 is:', tzar[['test.variable.3']], '\n')
@@ -16,6 +20,9 @@ cat('Value for test variable 4 is:', tzar[['test.variable.4']], '\n')
 cat('The working dir is', getwd(), '\n')
 cat('PAR.testing.output.filename=', tzar[['PAR.testing.output.filename']], '\n')
 
-test.text <- rep(1:10) 
-write.table(test.text, tzar[['PAR.testing.output.filename']] )
 
+
+test.text <- rep(1:10) 
+
+write.table(test.text, tzar[['PAR.testing.output.filename']] )
+write.table(x, tzar[['PAR.testing.output.filename2']] )
