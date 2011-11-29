b99f75200b7efd19173d311bdc16c98284cfd1b7
burn2shine@gmail.com <burn2shine@gmail.com@0edbd1c7-074e-7333-919a-b20e77d7077a>
2011-11-29 01:29:20 +0000

    Update python model code in all projects to use logging correctly

--- trunk/projects/guppy/model.py
+++ trunk/projects/guppy/model.py
@@ -1,17 +1,14 @@
 import glob
-import logging
 import os
 
 import basemodel
-
-logger = logging.getLogger('model')
 
 class Model(basemodel.BaseModel):
     def execute(self, runparams):
 
         variables = runparams.variables
 
-        logger.info("Executing model.py for the Guppy project")
+        self.logger.info("Executing model.py for the Guppy project")
 
         # this is for testing the repetitions file
         if variables['PAR.variable.to.test.repetitions'] > 0:
@@ -22,9 +19,9 @@ class Model(basemodel.BaseModel):
         #self.run_r_code( "example.R", runparams )
 
         # run Maxent
-        logger.info( "\n--> Running maxent" )
+        self.logger.info( "\n--> Running maxent" )
         self.run_r_code( "run.maxent.R", runparams )
 
         # run Zonation
-        logger.info( "\n--> Running zonation" )
+        self.logger.info( "\n--> Running zonation" )
         self.run_r_code( "run.zonation.R", runparams )
