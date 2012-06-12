dc0b5f743538b79839c81cb6cfbcde7df4b5ae46
burn2shine@gmail.com <burn2shine@gmail.com@0edbd1c7-074e-7333-919a-b20e77d7077a>
2012-06-12 05:52:55 +0000

    Added nectar user data and changes to start / stop scripts.

---
+++ trunk/scripts/tzar.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -n "Starting tzar node client:"
+
+export TZAR_DB="jdbc:postgresql://arcs-01.ivec.org:5432/rdv?user=rdv&password=YRxGRhq5"
+
+export TZAR_DIR="/home/ubuntu/tzar"
+if [ ! -d  $TZAR_DIR ]; then
+  mkdir $TZAR_DIR
+fi
+
+# run tzar as rdv role user
+java -jar /home/ubuntu/bin/rdv.jar pollandrun --runnerclass JythonRunner --svnurl=https://rdv-framework.googlecode.com/svn/trunk/ &
+
+# write the process id of the running process to a file
+echo $! > $TZAR_DIR/tzar.pid
