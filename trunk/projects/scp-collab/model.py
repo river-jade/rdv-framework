f19d88090cfa0da262a4e859dd665ddd74569fda
ascelin <ascelin@0edbd1c7-074e-7333-919a-b20e77d7077a>
2012-08-28 08:18:52 +0000

    add new project scp-collab

---
+++ trunk/projects/scp-collab/model.py
@@ -0,0 +1,27 @@
+import glob
+import os
+
+import basemodel
+
+class Model(basemodel.BaseModel):
+    def execute(self, runparams):
+
+        variables = runparams.variables
+
+        # self.logger.fine("\n--> Running maxent")
+        # this is for testing the repetitions file
+        # if variables['PAR.variable.to.test.repetitions'] > 0:
+        #   self.logger.fine("Now Doing repetitions, PAR.variable.to.test.repetitions=%s" % \
+        #         variables['PAR.variable.to.test.repetitions'])
+
+        # test R code
+        #self.run_r_code( "example.R", runparams )
+
+        # run Maxent
+        #self.logger.fine("\n--> Running maxent")
+        #self.run_r_code( "run.maxent.R", runparams )
+
+
+        # run Zonation
+        self.logger.fine("\n--> Running zonation")
+        self.run_r_code( "run.zonation.R", runparams )
