8c3bac5022a5710a64faa8d5e061f56b507e393a
burn2shine@gmail.com <burn2shine@gmail.com@0edbd1c7-074e-7333-919a-b20e77d7077a>
2013-11-03 23:08:10 +0000

    Update nectar node script to use tzar v0.4.2.

--- scripts/tzar.sh
+++ scripts/tzar.sh
@@ -9,8 +9,10 @@ fi
 
 # run tzar
 java -jar /home/ubuntu/bin/tzar.jar pollandrun \
-    --svnurl=https://rdv-framework.googlecode.com/svn/trunk/ \
-    --scpoutputhost=glass.eres.rmit.edu.au --scpoutputpath=/mnt/rdv/tzar_output \
+    --repository-prefixes=http://rdv-framework.googlecode.com/svn/trunk/,https://rdv-framework.googlecode.com/svn/trunk/,http://tzar-framework.googlecode.com/svn/trunk/,https://tzar-framework.googlecode.com/svn/trunk/ \
+    --scpoutputhost=glass.eres.rmit.edu.au \
+    --scpoutputpath=/mnt/rdv/tzar_output \
+    --scpoutputuser=ubuntu \
     --pemfile=/home/ubuntu/glass.pem $EXTRA_TZAR_FLAGS &
 
 # write the process id of the running process to a file
