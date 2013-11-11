bc68739a4e72d80037996961734475dcea28f349
burn2shine@gmail.com <burn2shine@gmail.com@0edbd1c7-074e-7333-919a-b20e77d7077a>
2013-11-11 02:52:25 +0000

    Update example.R to work with new code.

--- trunk/R/example.R
+++ trunk/R/example.R
@@ -8,15 +8,15 @@ for (i in 1:20) {
   Sys.sleep(.005)
 } 
 
-cat('Value for test variable 1 is:', test.variable.1, '\n')
-cat('Value for test variable 2 is:', test.variable.2, '\n')
-cat('Value for test variable 3 is:', test.variable.3, '\n')
-cat('Value for test variable 4 is:', test.variable.4, '\n')
+cat('Value for test variable 1 is:', variables$test.variable.1, '\n')
+cat('Value for test variable 2 is:', variables$test.variable.2, '\n')
+cat('Value for test variable 3 is:', variables$test.variable.3, '\n')
+cat('Value for test variable 4 is:', variables$test.variable.4, '\n')
 
 cat('The working dir is', getwd(), '\n')
-cat('PAR.testing.output.filename=', PAR.testing.output.filename, '\n')
+cat('test.output.filename=', outputFiles$test.output.filename, '\n')
 
 
 test.text <- rep(1:10) 
-write.table(test.text, PAR.testing.output.filename )
+write.table(test.text, outputFiles$test.output.filename )
 
