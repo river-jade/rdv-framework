69910e28e3536bb1207bd9799315cfb48fd63bf7
ascelin <ascelin@0edbd1c7-074e-7333-919a-b20e77d7077a>
2013-11-15 04:53:32 +0000

    adding basic functionality to ecotas project

--- projects/ecotas/model.R
+++ projects/ecotas/model.R
@@ -1,33 +1,65 @@
 # R file for testing the output during dry runs
 
-# an example of sourcing another R doc
-#source( "w.R" )
+# define matrix to store the pop trajs 
+
+pop.traj <- matrix( ncol=variables$no.timesteps+1, nrow=variables$reps )
+pop.traj[,1] <- variables$init.pop.size
+
+pop.traj.log <- matrix( ncol=variables$no.timesteps+1, nrow=variables$reps )
+pop.traj.log[,1] <- log(variables$init.pop.size)
 
 
-for (i in 1:20) {
-  cat('A sample output line: ', i, '\n')
-  Sys.sleep(.005)
+for( r in 1:variables$reps ) {
+    
+    eta <- rnorm(variables$no.timesteps, mean=0, sd=variables$sd )
+    for( t in 1:(variables$no.timesteps) ) {
+
+        pop.traj[r, t+1] <- pop.traj[r,t] * exp(variables$mu) * exp( eta[t] )
+        pop.traj.log[r, t+1] <- pop.traj.log[r,t] + variables$mu + eta[t]
+        #browser()
+    }
+
 }
 
-x <- matrix( nrow=20, ncol=20)
-x[] <- 1:(20*20)
 
-cat('Value for test variable 1 is:', variables$'test.variable.1', '\n')
-cat('Value for test variable 2 is:', variables$'test.variable.2', '\n')
-cat('Value for test variable 3 is:', variables$'test.variable.3', '\n')
-cat('Value for test variable 4 is:', variables$'test.variable.4', '\n')
 
-cat('The working dir is', getwd(), '\n')
-cat('PAR.testing.output.filename=', outputFiles$'PAR.testing.output.filename', '\n')
+       #------------------------
+       #  Write output files
+       #-------------------------
 
-cat('\n\n##The current working dir is', getwd(), '\n\n' )
+cat( '\nRun id =', variables$run.id )
+cat( "\noutput file is", outputFiles$output.plot, '\n' )
+cat( "\noutput R dump file is", outputFiles$output.dump, '\n' )
 
-test.text <- rep(1:10)
+# write a graph of a single relisatin (the 1st)
+pdf( paste( outputFiles$output.plot, '.single.pdf', sep='' ) )
+plot( 0:variables$no.timesteps, pop.traj.log[1,], type='l', ylim=c(2,8),
+     xlab="Time (years)", ylab="Log of population size" )
+dev.off()
 
-write.table(test.text, outputFiles$'PAR.testing.output.filename' )
-write.table(x, outputFiles$'PAR.testing.output.filename2' )
-str(outputFiles$'PAR.testing.output.filename')
+# now make a plot showing all realisations
+pdf( paste(outputFiles$output.plot, 'multiple.pdf',sep='') )
 
-str(inputFiles)
-str(outputFiles)
-str(variables)
+for( r in 1:variables$reps) {
+    if( r==1 ) plot( 0:variables$no.timesteps, pop.traj.log[r,], type='l', ylim=c(2,8),
+            xlab="Time (years)", ylab="Log of population size" )
+    else lines (0:variables$no.timesteps, pop.traj.log[r,], type='l' )
+}
+for( r in 1:variables$reps) {
+    if( r==1 ) plot( 0:variables$no.timesteps, pop.traj[r,], type='l', ylim=c(0,1200),
+            xlab="Time (years)", ylab="Population size" )
+    else lines (0:variables$no.timesteps, pop.traj[r,], type='l' )
+}
+
+dev.off()
+
+dump( "pop.traj", outputFiles$output.dump )
+
+
+
+## pdf(outputFiles$output.plot.pdf)
+## plot( 0:50, pop.traj, type='l' )
+## dev.off()
+
+
+#dev.copy( png, outputFiles$output.plot); dev.off()
