338ee6992a08dfac8bd1be993b80e5edc88abccb
burn2shine@gmail.com <burn2shine@gmail.com@0edbd1c7-074e-7333-919a-b20e77d7077a>
2013-12-05 06:12:58 +0000

    Remove input_files, output_files etc from project.yaml (and rename project_params.yaml to project.yaml)

--- R/example.R
+++ R/example.R
@@ -8,15 +8,15 @@ for (i in 1:20) {
   Sys.sleep(.005)
 } 
 
-cat('Value for test variable 1 is:', variables$test.variable.1, '\n')
-cat('Value for test variable 2 is:', variables$test.variable.2, '\n')
-cat('Value for test variable 3 is:', variables$test.variable.3, '\n')
-cat('Value for test variable 4 is:', variables$test.variable.4, '\n')
+cat('Value for test variable 1 is:', parameters$test.variable.1, '\n')
+cat('Value for test variable 2 is:', parameters$test.variable.2, '\n')
+cat('Value for test variable 3 is:', parameters$test.variable.3, '\n')
+cat('Value for test variable 4 is:', parameters$test.variable.4, '\n')
 
 cat('The working dir is', getwd(), '\n')
-cat('test.output.filename=', outputFiles$test.output.filename, '\n')
+cat('test.output.filename=', parameters$test.output.filename, '\n')
 
 
 test.text <- rep(1:10) 
-write.table(test.text, outputFiles$test.output.filename )
+write.table(test.text, parameters$test.output.filename )
 
