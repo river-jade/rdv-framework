776ad655138b0c29f7dc681ffa67892f5b982a1a
ascelin <ascelin@0edbd1c7-074e-7333-919a-b20e77d7077a>
2013-12-17 04:32:09 +0000

    updating ecotas project to run with new version of tzar

--- trunk/projects/ecotas/model.R
+++ trunk/projects/ecotas/model.R
@@ -2,20 +2,20 @@
 
 # define matrix to store the pop trajs 
 
-pop.traj <- matrix( ncol=variables$no.timesteps+1, nrow=variables$reps )
-pop.traj[,1] <- variables$init.pop.size
+pop.traj <- matrix( ncol=parameters$no.timesteps+1, nrow=parameters$reps )
+pop.traj[,1] <- parameters$init.pop.size
 
-pop.traj.log <- matrix( ncol=variables$no.timesteps+1, nrow=variables$reps )
-pop.traj.log[,1] <- log(variables$init.pop.size)
+pop.traj.log <- matrix( ncol=parameters$no.timesteps+1, nrow=parameters$reps )
+pop.traj.log[,1] <- log(parameters$init.pop.size)
 
 
-for( r in 1:variables$reps ) {
+for( r in 1:parameters$reps ) {
     
-    eta <- rnorm(variables$no.timesteps, mean=0, sd=variables$sd )
-    for( t in 1:(variables$no.timesteps) ) {
+    eta <- rnorm(parameters$no.timesteps, mean=0, sd=parameters$sd )
+    for( t in 1:(parameters$no.timesteps) ) {
 
-        pop.traj[r, t+1] <- pop.traj[r,t] * exp(variables$mu) * exp( eta[t] )
-        pop.traj.log[r, t+1] <- pop.traj.log[r,t] + variables$mu + eta[t]
+        pop.traj[r, t+1] <- pop.traj[r,t] * exp(parameters$mu) * exp( eta[t] )
+        pop.traj.log[r, t+1] <- pop.traj.log[r,t] + parameters$mu + eta[t]
         #browser()
     }
 
@@ -50,61 +50,61 @@ var.traj <- apply( pop.traj, 2, var)
        #  Write output files
        #-------------------------
 
-cat( '\nRun id =', variables$run.id )
-cat( "\noutput file is", outputFiles$output.plot, '\n' )
-cat( "\noutput R dump file is", outputFiles$output.dump, '\n' )
+cat( '\nRun id =', parameters$run.id )
+cat( "\noutput file is", parameters$output.plot, '\n' )
+cat( "\noutput R dump file is", parameters$output.dump, '\n' )
 
 # write a graph of a single relisatin (the 1st)
-pdf( paste( outputFiles$output.plot, '.single.pdf', sep='' ) )
-plot( 0:variables$no.timesteps, pop.traj.log[1,], type='l', ylim=c(2,8),
+pdf( paste( parameters$output.plot, '.single.pdf', sep='' ) )
+plot( 0:parameters$no.timesteps, pop.traj.log[1,], type='l', ylim=c(2,8),
      xlab="Time (years)", ylab="Log of population size" )
 dev.off()
 
 # now make a plot showing all realisations
-pdf( paste(outputFiles$output.plot, 'multiple.pdf',sep='') )
+pdf( paste(parameters$output.plot, 'multiple.pdf',sep='') )
 
-for( r in 1:variables$reps) {
-    if( r==1 ) plot( 0:variables$no.timesteps, pop.traj.log[r,], type='l', ylim=c(0,8),
+for( r in 1:parameters$reps) {
+    if( r==1 ) plot( 0:parameters$no.timesteps, pop.traj.log[r,], type='l', ylim=c(0,8),
             xlab="Time (years)", ylab="Log of population size" )
-    else lines (0:variables$no.timesteps, pop.traj.log[r,], type='l' )
+    else lines (0:parameters$no.timesteps, pop.traj.log[r,], type='l' )
 }
 # plot the mean line
-lines(0:variables$no.timesteps, mean.traj.log, type='l', col='red', lwd=4  )
+lines(0:parameters$no.timesteps, mean.traj.log, type='l', col='red', lwd=4  )
 
 # show the version without the log transformation
-for( r in 1:variables$reps) {
-    if( r==1 ) plot( 0:variables$no.timesteps, pop.traj[r,], type='l', ylim=c(0,1200),
+for( r in 1:parameters$reps) {
+    if( r==1 ) plot( 0:parameters$no.timesteps, pop.traj[r,], type='l', ylim=c(0,1200),
             xlab="Time (years)", ylab="Population size" )
-    else lines (0:variables$no.timesteps, pop.traj[r,], type='l')
+    else lines (0:parameters$no.timesteps, pop.traj[r,], type='l')
 }
 # plot the mean line
-lines(0:variables$no.timesteps, mean.traj, type='l', col='red', lwd=4  )
+lines(0:parameters$no.timesteps, mean.traj, type='l', col='red', lwd=4  )
 
 dev.off()
 
 # Dumpt all the matrix with all the poptrajectories. 
-dump( "pop.traj", outputFiles$output.dump )
+dump( "pop.traj", parameters$output.dump )
 
 
 
-glob.output.file <- paste( variables$global.output.dir, '/', variables$global.output.filename, sep='')
+glob.output.file <- paste( parameters$global.output.dir, '/', parameters$global.output.filename, sep='')
 
 
 column.names <- c('runId', 'initPopSize', 'mu', 'sd', 'reps', 'median', 'medianLog', 'var', 'varLog',
                   'min', 'minLog')
 
 line.to.paste <- c(
-    variables$run.id,
-    variables$init.pop.size,
-    variables$mu,
-    variables$sd,
-    variables$reps,
-    round(median.traj[variables$timeToEvalPopStats],2),
-    round(median.traj.log[variables$timeToEvalPopStats],2),
-    round(var.traj[variables$timeToEvalPopStats],2),
-    round(var.traj.log[variables$timeToEvalPopStats],2),
-    round(min.traj[variables$timeToEvalPopStats],2),
-    round(min.traj.log[variables$timeToEvalPopStats],2)
+    parameters$run.id,
+    parameters$init.pop.size,
+    parameters$mu,
+    parameters$sd,
+    parameters$reps,
+    round(median.traj[parameters$timeToEvalPopStats],2),
+    round(median.traj.log[parameters$timeToEvalPopStats],2),
+    round(var.traj[parameters$timeToEvalPopStats],2),
+    round(var.traj.log[parameters$timeToEvalPopStats],2),
+    round(min.traj[parameters$timeToEvalPopStats],2),
+    round(min.traj.log[parameters$timeToEvalPopStats],2)
     )
 
 if(  !file.exists(glob.output.file ) ) {
@@ -114,10 +114,10 @@ cat( line.to.paste, '\n', file=glob.output.file, append=TRUE)
 
 
 
-## pdf(outputFiles$output.plot.pdf)
+## pdf(parameters$output.plot.pdf)
 ## plot( 0:50, pop.traj, type='l' )
 ## dev.off()
 
-#c(variables$run.id, mean.traj.log, 
+#c(parameters$run.id, mean.traj.log, 
 
-#dev.copy( png, outputFiles$output.plot); dev.off()
+#dev.copy( png, parameters$output.plot); dev.off()
