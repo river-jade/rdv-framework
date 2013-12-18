f3bdabfa2b6bca7189ff5514c685205fb9eba20d
doctorluz@gmail.com <doctorluz@gmail.com@0edbd1c7-074e-7333-919a-b20e77d7077a>
2013-12-18 06:25:05 +0000

    Added some R code to read downloaded files

--- projects/EMS-paper-eg/model.R
+++ projects/EMS-paper-eg/model.R
@@ -1,9 +1,20 @@
 # R file for testing the output during dry runs
+library("rgdal")
 
+cat( "\n*** The path to the downloaded GBIF data for a single frog species is:",  parameters$path.to.GBIF.data, '\n' )
+frog_lyr<-ogrListLayers(dsn=parameters$path.to.GBIF.data)
+frog_points <- readOGR(dsn=parameters$path.to.GBIF.data, layer=frog_lyr)
 
+cat( "\n*** The path to the DSE data for protected areas in GML format is:",  parameters$path.to.crown.land.kml, '\n' )
+crown_lyr<-ogrListLayers(dsn=parameters$path.to.crown.land.kml)
+crown_points <- readOGR(dsn=parameters$path.to.crown.land.kml, layer=crown_lyr)
 
-cat( "\n*** The path to the downloaded GBIF data is:",  parameters$path.to.GBIF.data, '\n' )
-cat( "\n*** The path to spp range shp file dir is :",  parameters$path.to.spp.ranges.shp, '\n' )
+cat( "\n*** The path to Anuran species range shp file dir is :",  parameters$path.to.spp.ranges.shp, '\n' )
+shapefileName <- c(parameters$path.to.spp.ranges.shp,"\ANURA.shp")
+cat( "\n*** The path to the actual shapefile should be :",  shapefileName , '\n' )
+
+sp_lyr<-ogrListLayers(dsn=shapefileName )
+sp_polys <- readOGR(dsn=shapefileName , layer=sp_lyr)
 
 cat('Value for test variable 1 is:', parameters$'test.variable.1', '\n')
 
