ce381021dc4cf63c65279ed8f7c444b0cd8c0a63
ascelin <ascelin@0edbd1c7-074e-7333-919a-b20e77d7077a>
2013-12-19 01:21:41 +0000

    adding R code to read GIS files to the EMS-paper-eg prject

--- projects/EMS-paper-eg/model.R
+++ projects/EMS-paper-eg/model.R
@@ -10,7 +10,7 @@ crown_lyr<-ogrListLayers(dsn=parameters$path.to.crown.land.kml)
 crown_points <- readOGR(dsn=parameters$path.to.crown.land.kml, layer=crown_lyr)
 
 cat( "\n*** The path to Anuran species range shp file dir is :",  parameters$path.to.spp.ranges.shp, '\n' )
-shapefileName <- c(parameters$path.to.spp.ranges.shp,"\ANURA.shp")
+shapefileName <- parameters$path.to.spp.ranges.shp
 cat( "\n*** The path to the actual shapefile should be :",  shapefileName , '\n' )
 
 sp_lyr<-ogrListLayers(dsn=shapefileName )
