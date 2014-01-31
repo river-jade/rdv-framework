7acae19fa562e8abe92776e0b449e75822b933d2
langfob@gmail.com <langfob@gmail.com@0edbd1c7-074e-7333-919a-b20e77d7077a>
2014-01-31 06:30:23 +0000

    g2: Added verbose version of getEnvLayers() to copy environment layers to the tzar output area (or wherever else you point it).

--- projects/g2/g2.R
+++ projects/g2/g2.R
@@ -99,10 +99,80 @@ cat ("\n\n")
 
 #===============================================================================
 
+    #  This method of copying a list of files is based on:
+    #  http://stackoverflow.com/questions/2384517/using-r-to-copy-files
+
+getEnvLayers = function (srcDir, targetDir, 
+                         filespec = "*.asc", 
+                         verbose=TRUE, 
+                         dirSlash = dir.slash)
+    {
+        #-----------------------------------------------------------------------
+        #  Get the list of files to copy from, then copy them to the target dir.
+        #-----------------------------------------------------------------------
+    
+    srcFullNameList = list.files (srcDir, filespec, full.names = TRUE)
+    if (verbose) print (srcFullNameList)
+    
+    file.copy (srcFullNameList, targetDir)
+    
+        #--------------------------------------------
+        #  Check to make sure that the copy worked.
+        #--------------------------------------------
+    
+    srcBaseNameList = list.files (srcDir, filespec, full.names = FALSE)
+    if (verbose) 
+        {
+        cat ("\n\nsrcBaseNameList = \n")
+        print (srcBaseNameList)
+        }
+    
+    targetBaseNameList = list.files (targetDir, filespec, full.names = FALSE)
+    if (verbose) 
+        {
+        cat ("\n\ntargetBaseNameList = \n")
+        print (targetBaseNameList)
+        }
+    
+    cat ("\n\n============  About to test match  ===============")
+    fileListMatchTests = (srcBaseNameList == targetBaseNameList)
+#    fileListMatchTests [4] = FALSE  #  To force test of mismatch...
+    cat ("\n\n  ----------  fileListMatchTests = ", fileListMatchTests, "\n\n")
+    if (length (fileListMatchTests) != sum (fileListMatchTests))
+        {
+        cat ("\n\n***** QUITTING:  Source and target file lists don't match in getEnvLayers(). *****")
+        cat ("\n", fileListMatchTests, "\n\n")
+        quit()        
+        } 
+    
+    if (verbose) cat ("\n\nsrcBaseNameList DOES EQUAL targetBaseNameList.")
+    
+        #---------------------------------------------------------------------
+        #  Make the list of new files available downstream.
+        #  Not sure if this will be necessary, but I have it in hand right 
+        #  now so I'll return it.  Can always remove it later if not needed.
+        #---------------------------------------------------------------------
+    
+    targetFullNameList = list.files (targetDir, filespec, full.names = TRUE)
+    if (verbose) 
+        {
+        cat ("\n\ntargetFullNameList = \n")        
+        print (targetFullNameList)
+        }
+    
+    return (targetFullNameList)
+    }
+
+#===============================================================================
+
 	#--------------------------------
 	#  Get environment layers.
 	#--------------------------------
 
+envLayersDir                  = "/Users/Bill/D/Projects_RMIT/AAA_PapersInProgress/G01 - simulated_ecology/MaxentTests/MattsVicTestLandscape/MtBuffaloEnvVars_Originals/"
+curFullMaxentEnvLayersDirName = "/Users/Bill/tzar/outputdata/g2/default_runset/400_Scen_1/InputEnvLayers"
+
+getEnvLayers (envLayersDir, curFullMaxentEnvLayersDirName)
 
 	#--------------------------------------------
 	#  Get true species distributions.
