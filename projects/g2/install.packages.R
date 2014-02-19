5cb072144c2f7a25b3b83785141baeb720d05967
langfob@gmail.com <langfob@gmail.com@0edbd1c7-074e-7333-919a-b20e77d7077a>
2014-02-19 00:06:47 +0000

    g2: Copied Ascelin's code for installing all required packages that is useful for setting up nectar nodes etc under tzar.  Not running it yet and it needs to be modified for g2, but this is the beginning of that.

---
+++ projects/g2/install.packages.R
@@ -0,0 +1,27 @@
+## Some notes below about how to check if a library is already
+## installed Might also want to think about the case where it is
+## installed but not for the current vetrsion of R. - AG 2011.10.31
+
+## # defining this one line function does the trick
+## is.installed <- function(mypkg) is.element(mypkg, installed.packages()[,1])
+
+## # test:
+
+## > is.installed ("rjson" )
+## [1] TRUE
+
+## > is.installed ("abc" )
+## [1] FALSE
+
+
+## Ref: http://r.789695.n4.nabble.com/test-if-a-package-is-installed-td1750671.html#a1750674
+
+
+
+install.packages('RSQLite', repos='http://cran.r-project.org')
+install.packages('maptools', repos='http://cran.r-project.org')
+install.packages('msm', repos='http://cran.r-project.org')
+install.packages('optparse',  repos='http://cran.r-project.org')
+install.packages('rjson',  repos='http://cran.r-project.org')
+install.packages('pixmap',  repos='http://cran.r-project.org')
+
