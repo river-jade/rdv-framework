bce8db9ecda47066be7ae826ff5e1de1af7be4f9
burn2shine@gmail.com <burn2shine@gmail.com@0edbd1c7-074e-7333-919a-b20e77d7077a>
2014-03-06 05:39:08 +0000

    Fix bug in start nectar node where tzar directory was not being created.

--- scripts/start-nectar-node.sh
+++ scripts/start-nectar-node.sh
@@ -22,6 +22,7 @@ wget http://rdv-framework.googlecode.com/svn/trunk/scripts/tzar -O /usr/local/bi
 chmod +x /usr/local/bin/tzar
 
 cd /home/ubuntu
+mkdir /home/ubuntu/tzar
 
 # echo "HostKeyAlgorithms ssh-rsa" >> /etc/ssh/ssh_config
 echo "|1|vsNQtnEsu8UD1ivBbbVmz9n/PI0=|AIFdBpKKfm+tYWPsecBdeqE/GL4= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA3v38O1h9ZA7Fjbgi7yYJOOWxZWjkalm6fnDhtsUiApeAL7jbbBn0Am4JeDfC//Nfy2CthuBBYSGltsLDFfMRQ6sgGGRr4bHBngL6aIyAvKaBPSNpaIOQfG3IwinBtD1vP024kV6E38q62t2kdqpl6i7s0cHWCpexylrFkEtDUAnQ2OEeStMXtAdz13Czto2cu0mPm4e19OlRhofv5tEHR1n4wo+0lVnGi4lJCNqeT7VyzzPGhQygpUqJ3yv5VjQq5rOQfBzuxONyPssU4RcA8PDvcUmS4EHnBf0rl76UmbYtXDonMVcsAeTmQ9NJilzHjcF0kLUTuf1+AyTXsWgcdQ==
