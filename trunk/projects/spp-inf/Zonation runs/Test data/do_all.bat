e1fed4f072f0ae790adc01218e36d02eb3d36e49
heini.kujala@gmail.com <heini.kujala@gmail.com@0edbd1c7-074e-7333-919a-b20e77d7077a>
2014-03-27 05:58:58 +0000

    spp-inf: creating folders for Zonation runs

---
+++ trunk/projects/spp-inf/Zonation runs/Test data/do_all.bat
@@ -0,0 +1,7 @@
+call zig3 -r set.dat splist_basicSDM.spp output/out_basicSDM.txt 0.0 0 1.0 1
+call zig3 -r set.dat splist_scaled.spp output/out_scaled.txt 0.0 0 1.0 1
+call zig3 -r set.dat splist_thMTP.spp output/out_thMTP.txt 0.0 0 1.0 1
+call zig3 -r set.dat splist_th10TP.spp output/out_th10TP.txt 0.0 0 1.0 1
+call zig3 -r set.dat splist_thEqSS_SP.spp output/out_thEqSS_SP.txt 0.0 0 1.0 1
+call zig3 -r set.dat splist_thMaxSSSP.spp output/out_thMaxSSSP.txt 0.0 0 1.0 1
+
