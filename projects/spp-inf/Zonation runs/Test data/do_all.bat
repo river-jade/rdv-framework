62aabf5ac3a66e0831fe21addbb1daee08e84769
heini.kujala@gmail.com <heini.kujala@gmail.com@0edbd1c7-074e-7333-919a-b20e77d7077a>
2014-03-27 06:12:35 +0000

    spp-inf: setting up test runs

--- projects/spp-inf/Zonation runs/Test data/do_all.bat
+++ projects/spp-inf/Zonation runs/Test data/do_all.bat
@@ -1,7 +1,8 @@
-call zig3 -r set.dat splist_basicSDM.spp output/out_basicSDM.txt 0.0 0 1.0 1
-call zig3 -r set.dat splist_scaled.spp output/out_scaled.txt 0.0 0 1.0 1
-call zig3 -r set.dat splist_thMTP.spp output/out_thMTP.txt 0.0 0 1.0 1
-call zig3 -r set.dat splist_th10TP.spp output/out_th10TP.txt 0.0 0 1.0 1
-call zig3 -r set.dat splist_thEqSS_SP.spp output/out_thEqSS_SP.txt 0.0 0 1.0 1
-call zig3 -r set.dat splist_thMaxSSSP.spp output/out_thMaxSSSP.txt 0.0 0 1.0 1
-
+call zig3 -r set.dat splist_all.spp output/out_all.txt 0.0 0 1.0 1
+call zig3 -r set.dat splist_missingSp1.spp output/out_missingSp1.txt 0.0 0 1.0 1
+call zig3 -r set.dat splist_missingSp2.spp output/out_missingSp2.txt 0.0 0 1.0 1
+call zig3 -r set.dat splist_missingSp3.spp output/out_missingSp3.txt 0.0 0 1.0 1
+call zig3 -r set.dat splist_missingSp4.spp output/out_missingSp4.txt 0.0 0 1.0 1
+call zig3 -r set.dat splist_missingSp5.spp output/out_missingSp5.txt 0.0 0 1.0 1
+call zig3 -r set.dat splist_missingSp6.spp output/out_missingSp6.txt 0.0 0 1.0 1
+call zig3 -r set.dat splist_missingSp7.spp output/out_missingSp7.txt 0.0 0 1.0 1
