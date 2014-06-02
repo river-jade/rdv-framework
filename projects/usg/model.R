2d5fc3664379e39512365e3aa3ac3220f83b2a96
ascelin <ascelin@0edbd1c7-074e-7333-919a-b20e77d7077a>
2014-06-02 11:03:02 +0000

    Making first draft of usg script

---
+++ projects/usg/model.R
@@ -0,0 +1,20 @@
+# R file for testing the output during dry runs
+
+
+p <- parameters
+
+
+# build the commandline to run zonation
+
+z.cmdline <- paste( p$z.executable, p$z.settings.file, p$z.spp.file, p$z.output.file,
+                   p$z.other.cmd.line.args )
+
+cat( '\n\nThe commandline to run zonation is:\n', z.cmdline )
+
+
+cat( '\n\nTrying to run zonation....')
+
+
+#system2( system.command, args=system.command.arguments, env="DISPLAY=:1" )      
+#system( z.cmdline )
+
