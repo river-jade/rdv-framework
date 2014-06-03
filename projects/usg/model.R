b4c91252ab383628667c8475274b160068148322
ascelin <ascelin@0edbd1c7-074e-7333-919a-b20e77d7077a>
2014-06-03 02:56:33 +0000

    usg: updating to generate zonation spp file on the fly

--- projects/usg/model.R
+++ projects/usg/model.R
@@ -8,18 +8,26 @@ p <- parameters
     # Generate the species list file for zonation 
     #--------------------------------------------
 
-cat( '\np$z.input.data.dir = ',  p$z.input.data.dir )
-cat( '\np$z.spp.list.filename = ',  p$z.spp.list.filename )
+cat( '\nThe input dir for species data is:',  p$z.input.data.dir )
+cat( '\nThe files name for the Zonation spp file is:',  p$z.spp.list.filename )
 
 # Get a list of all the .asc in the input dir. These will all go into
 # the input species file
 spp.file.list <- dir( p$z.input.data.dir, pattern='asc', full.names=TRUE )
+
+if( length(spp.file.list) == 0 ) {cat('\nERROR: No species maps were found, stopping.\n\n'); stop()}
+
 
 # Now write the the zonation species list file (assuming all spp have
 # the same weight for now
 for( s in spp.file.list){
     cat( '1.0 1.0 1 1 1', s, '\n', append=TRUE, file=p$z.spp.list.filename )
 }
+
+
+cat( '\n #########length of s is', length(spp.file.list) )
+
+
 
     #--------------------------------------------
     # Run Zonation
