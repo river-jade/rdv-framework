af40345c80b93dea8a545f12e172086d88dbd808
langfob@gmail.com <langfob@gmail.com@0edbd1c7-074e-7333-919a-b20e77d7077a>
2014-09-21 02:16:50 +0000

    rdvPackages: First commit of rdvPackages.  Starting to create packages for rdvfileio and reserveselection.  rdvfileio is in a little better shape than reserveselection.  I started getting reserveselection going, but had some issues with make.rectangular.planning.units so I am starting over on that so that I can track incremental changes in the source up until it breaks.

---
+++ projects/rdvPackages/rdvfileio/R/rdvfileio.R
@@ -0,0 +1,15 @@
+#' Functions to read and write matrices to 3 simple image file types.
+#'
+#' rdvfileio provides function calls to read and write matrices with 3 file
+#' types: pgm files (.pgm), plain ascii text files (.txt), and esri format
+#' ascii files (.asc).
+#'
+#' The main functions you're likely to need from \pkg{rdvfileio} are
+#' \code{\link{write.pgm.file}}, \code{\link{write.txt.file}}, and
+#' \code{\link{write.asc.file}}, though there are several variants that write
+#' combinations of two or three of the file types.  See the vignette for more
+#' details.
+#'
+#' @docType package
+#' @name rdvfileio
+NULL
---
+++ projects/rdvPackages/reserveselection/tests/test-all.R
@@ -0,0 +1,10 @@
+
+#  test_package ("reserveselection")
+
+library ("rdvfileio")
+library ("reserveselection")
+library ("testthat")
+
+#source ("tests/testthat/test.make.rectangular.planning.units.R")
+
+test_package ("reserveselection")
