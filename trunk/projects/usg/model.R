b9b1f8b87ecbc925e34cc83d20ddbbcf842a2172
ayeshatulloch@gmail.com <ayeshatulloch@gmail.com@0edbd1c7-074e-7333-919a-b20e77d7077a>
2014-09-29 04:43:29 +0000

    revised model.R for only 10 sp

--- trunk/projects/usg/model.R
+++ trunk/projects/usg/model.R
@@ -38,13 +38,13 @@ if( length(spp.file.list) == 0 ) {
 
 #note: for retention analyses, the spp file has to have 2 copies of the species names, so repeat the about command if running retention analyses:
 ctr <- 1
-for( s in spp.file.list){
+for( s in spp.file.list[1:10]){
     #cat( '1.0 ', x[ctr] ' 1 1 1 ', s, '\n', append=TRUE, file=p$z.spp.list.filename, sep='' )
     cat( '1.0 1.0 1 1 1 ', s, '\n', append=TRUE, file=p$z.spp.list.filename, sep='' )
 	ctr <- ctr + 1
 }
 ctr <- 1
-for( s in spp.file.list){
+for( s in spp.file.list[1:10]){
     #cat( '1.0 ', x[ctr] ' 1 1 1 ', s, '\n', append=TRUE, file=p$z.spp.list.filename, sep='' )
     cat( '1.0 1.0 1 1 1 ', s, '\n', append=TRUE, file=p$z.spp.list.filename, sep='' )
 	ctr <- ctr + 1
