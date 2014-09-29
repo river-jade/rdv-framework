945ed80d7978a774e7d4f74489adc5c17b01e5e6
langfob@gmail.com <langfob@gmail.com@0edbd1c7-074e-7333-919a-b20e77d7077a>
2014-09-29 01:50:45 +0000

    rdvPackages, biodivprobgen: First addition of the package directory and oldest version of the set cover code R code.

---
+++ trunk/projects/rdvPackages/biodivprobgen/R/biodivprobgen-package.R
@@ -0,0 +1,13 @@
+#' Generate problems for testing biodiversity algorithms.
+#'
+#' The \pkg{biodivprobgen} package is intended to make it possible to generate 
+#' problems of varying difficulty, generally with known solutions and 
+#' sometimes, with known degrees of difficulty. 
+#' 
+#' "Problems" are characterized by things like the spatial distribution of 
+#' species on the landscape, the rarity distribution of species, the 
+#' heterogeneity of input features such as land use and climate, etc.
+#' 
+#' @docType package
+#' @name biodivprobgen
+NULL
