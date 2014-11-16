65b248adbc772f24f257ff077ec560f56974473e
langfob@gmail.com <langfob@gmail.com@0edbd1c7-074e-7333-919a-b20e77d7077a>
2014-11-16 04:50:51 +0000

    rdvpackages: committing a couple of uncommitted (and not that important) files before setting up replacement mac.

--- trunk/projects/rdvPackages/biodivprobgen/R/setCoverGenerator.R
+++ trunk/projects/rdvPackages/biodivprobgen/R/setCoverGenerator.R
@@ -14,46 +14,7 @@
 
 #===============================================================================
 
-calculate_num_possible_solutions = function (num_PUs)
-    {
-    sum (choose (num_PUs, 1:num_PUs))
-    }
-
-#===============================================================================
-
-#  node_contents_dict
-#  node link
-
-#===============================================================================
-
-    #  Link 2 nodes
-    #  A link is just a 2 item vector of then node IDs of the endpoints.
-    #  The node IDs are in increasing order.
-
-link_2_nodes = function (node1_ID, node2_ID)
-    {
-    c (min (node1_ID, node2_ID), max (node1_ID, node2_ID))
-    }
-
-#-------------------------------------------------------------------------------
-
-put_link_in_link_set_for_node = function (link_ID, node_ID, links_for_each_node)
-    {
-    last_row = dim (links_for_each_node) [1]
-    links_for_each_node [last_row + 1] =
-    }
-
-#-------------------------------------------------------------------------------
-
-put_link_in_end_node_sets = function (link_ID, node1_ID, node2_ID)
-    {
-    put_link_in_link_set_for_node (link_ID, node1_ID)
-    put_link_in_link_set_for_node (link_ID, node2_ID)
-    }
-
-#===============================================================================
-
-#  define initial option and derived values
+#  define initial option and derived values, particularly, array sizes
 #  create dependent set nodes
 #  build set of cliques given a set of clique IDs
     #  build clique given a set of node IDs
@@ -72,50 +33,8 @@ put_link_in_end_node_sets = function (link_ID, node1_ID, node2_ID)
                         #  link 2 nodes and put link in both nodes
 #  test that invariant properties hold
 
-
-
-
 #===============================================================================
-
-getSmallSubsetID = function (linkColl, linkID, smallSubsetIDcol)
-    { return (linkColl [linkID, smallSubsetIDcol]) }
-
-getLargeSubsetID = function (linkColl, linkID, largeSubsetIDcol)
-    { return (linkColl [linkID, largeSubsetIDcol]) }
-
-getGroupIDofSubset = function (groupIDofSubset, subsetID)
-    { return (groupIDofSubset [subsetID]) }
-
-    # For every subset pair whose two subsets are in the same group,
-    # put its index number in its two subsets, i.e., build cliques.
-
-buildCliques = function (numLinks, linkColl, #curLinkID,
-                         smallSubsetIDcol, largeSubsetIDcol,
-                         groupIDofSubset, subsetsColl)
-    {
-    for (curLinkID in 1:numLinks)
-        {
-        #  Get the subset IDs at the ends of this link.
-        smallSubsetID = linkColl [curLinkID, smallSubsetIDcol]
-        largeSubsetID = linkColl [curLinkID, largeSubsetIDcol]
-
-        #  Find the group ID for each of the 2 subsets.
-        smallSubsetGroupID = groupIDofSubset [smallSubsetID]
-        largeSubsetGroupID = groupIDofSubset [largeSubsetID]
-
-        #  If they're in the same group, add the link to both subsets.
-        if (smallSubsetGroupID == largeSubsetGroupID)
-            {
-            subsetsColl [[as.character(smallSubsetID)]] =
-                append (subsetsColl [[as.character(smallSubsetID)]], curLinkID)
-            subsetsColl [[as.character(largeSubsetID)]] =
-                append (subsetsColl [[as.character(largeSubsetID)]], curLinkID)
-            }
-        }
-
-    return (subsetsColl)
-    }
-
+#  define initial option and derived values, particularly, array sizes
 #===============================================================================
 
 library (hash)
@@ -163,6 +82,242 @@ cat ("\n\t\tnumSubsets = ", numSubsets)
 cat ("\n\t\tnumIntergroupLinkingRounds = ", numIntergroupLinkingRounds)
 cat ("\n\t\ttargetNumLinksBetweenGroups = ", targetNumLinksBetweenGroups)
 cat ("\n\n")
+
+#===============================================================================
+
+#  create dependent set nodes
+#  build set of cliques given a set of clique IDs
+    #  build clique given a set of node IDs
+        #  link all nodes in clique
+            #  link each node to all other nodes in clique
+                #  link a node to all other nodes in clique
+                    #  link 2 nodes and put link in both nodes
+#  create independent set nodes
+    #  attach each to a different clique
+                #  link a node to all other nodes in clique
+                    #  link 2 nodes and put link in both nodes
+#  do rounds of interclique linking
+    #  randomly choose two different cliques
+        #  do rounds of linking between the given clique pair
+            #  randomly choose a node pair, one node in each clique, without replacement
+                        #  link 2 nodes and put link in both nodes
+#  test that invariant properties hold
+
+#===============================================================================
+
+calculate_num_possible_solutions = function (num_PUs)
+    {
+    sum (choose (num_PUs, 1:num_PUs))
+    }
+
+#===============================================================================
+
+#  node_contents_dict
+#  node link
+
+#===============================================================================
+
+    #  Link 2 nodes
+    #  A link is just a 2 item vector of the node IDs of the endpoints.
+    #  The node IDs are in increasing order.
+
+link_2_nodes = function (node1_ID, node2_ID)
+    {
+    c (min (node1_ID, node2_ID), max (node1_ID, node2_ID))
+    }
+
+#-------------------------------------------------------------------------------
+
+put_link_in_link_set_for_node = function (link_ID, node_ID, links_for_each_node)
+    {
+    last_row = dim (links_for_each_node) [1]
+    #    links_for_each_node [last_row + 1] = 
+    }
+
+#-------------------------------------------------------------------------------
+
+put_link_in_end_node_link_sets = function (link_ID, node1_ID, node2_ID)
+    {
+    put_link_in_link_set_for_node (link_ID, node1_ID)
+    put_link_in_link_set_for_node (link_ID, node2_ID)
+    }
+
+#===============================================================================
+
+getSmallSubsetID = function (linkColl, linkID, smallSubsetIDcol)
+    { return (linkColl [linkID, smallSubsetIDcol]) }
+
+getLargeSubsetID = function (linkColl, linkID, largeSubsetIDcol)
+    { return (linkColl [linkID, largeSubsetIDcol]) }
+
+getGroupIDofSubset = function (groupIDofSubset, subsetID)
+    { return (groupIDofSubset [subsetID]) }
+
+    # For every subset pair whose two subsets are in the same group,
+    # put its index number in its two subsets, i.e., build cliques.
+
+buildCliques = function (numLinks, linkColl, #curLinkID,
+                         smallSubsetIDcol, largeSubsetIDcol,
+                         groupIDofSubset, subsetsColl)
+    {
+    for (curLinkID in 1:numLinks)
+        {
+        #  Get the subset IDs at the ends of this link.
+        smallSubsetID = linkColl [curLinkID, smallSubsetIDcol]
+        largeSubsetID = linkColl [curLinkID, largeSubsetIDcol]
+
+        #  Find the group ID for each of the 2 subsets.
+        smallSubsetGroupID = groupIDofSubset [smallSubsetID]
+        largeSubsetGroupID = groupIDofSubset [largeSubsetID]
+
+        #  If they're in the same group, add the link to both subsets.
+        if (smallSubsetGroupID == largeSubsetGroupID)
+            {
+            subsetsColl [[as.character(smallSubsetID)]] =
+                append (subsetsColl [[as.character(smallSubsetID)]], curLinkID)
+            subsetsColl [[as.character(largeSubsetID)]] =
+                append (subsetsColl [[as.character(largeSubsetID)]], curLinkID)
+            }
+        }
+
+    return (subsetsColl)
+    }
+
+#===============================================================================
+#  define initial option and derived values, particularly, array sizes
+#===============================================================================
+
+library (hash)
+
+seed = 17
+set.seed (seed)
+
+    #  In "simple model ..." paper, they use:
+    #       n in [8..100] while alpha = 0.52, r = 1
+    #       r in [0.8..2.5] while n = 20, alpha = 0.77
+    #       alpha in [0.2..1] while n = 20, r = 2.5
+    #  In all 3 cases, best fit to the theory was when the parameter
+    #  was at the top of its range.  See figure 1 in the paper.
+    #
+    #  Values in the paper for propOfLinksBetweenGroups were more variable.
+    #  See figures 2 and 3 and text in column 2 of page 4.
+    #  In those examples, they had:
+    #       Figure 2:   k=2, alpha=0.8, r=3, n in {20,30,40}
+    #                   p in [0.14..0.36] w/ critical value Pcr ~ 0.23
+    #    ???  What is "k" in Figure 2?
+
+# numGroups = 20
+# alpha = 0.8
+# propOfLinksBetweenGroups = 0.23
+# r_density = 3.0
+
+n__numGroups = 3                      #  num_cliques = 3
+alpha__ = 1
+p__propOfLinksBetweenGroups = 0.5    #  p__prop_of_links_between_cliques   
+									 #  not the right name?
+									 #  is it really the proportion of nodes in one clique 
+									 #  to link to another clique during one round?
+									 #  p__prop_of_nodes_in_clique_to_try_to_interlink_in_one_round?
+									 #  p__prop_to_link_between_two_cliques_in_one_round?
+									 #  p__prop
+												
+r__density = 0.5
+
+	#  num_nodes_per_clique = num_cliques ^ alpha
+numSubsetsPerGroup = numGroups ^ alpha    
+	#  num_nodes = num_cliques * num_nodes_per_clique
+numSubsets = numGroups ^ (alpha + 1)      #  i.e., numGroups * numSubsetsPerGroup
+
+numIntergroupLinkingRounds = r_density * numGroups * log (numGroups)
+
+	#  This is the target number for each linking round?
+	#  Is the total number of links generated over all rounds aiming for 
+	#  around target * numRounds?
+targetNumLinksBetweenGroups = propOfLinksBetweenGroups * (numGroups^alpha)
+								#  i.e., propOfLinksBetweenGroups * numSubsetsPerGroup
+
+cat ("\n\nInput variable settings")
+cat ("\n\t\tnumGroups = ", numGroups)
+cat ("\n\t\talpha = ", alpha)
+cat ("\n\t\tpropOfLinksBetweenGroups = ", propOfLinksBetweenGroups)
+cat ("\n\t\tr_density = ", r_density)
+
+cat ("\n\nDerived variable settings")
+cat ("\n\t\tnumSubsetsPerGroup = ", numSubsetsPerGroup)
+cat ("\n\t\tnumSubsets = ", numSubsets)
+cat ("\n\t\tnumIntergroupLinkingRounds = ", numIntergroupLinkingRounds)
+cat ("\n\t\ttargetNumLinksBetweenGroups = ", targetNumLinksBetweenGroups)
+cat ("\n\n")
+
+#=========================================
+
+	#  User-supplied control parameters.
+	
+n__num_cliques = 3
+alpha__ = 1
+p__prop_of_links_between_cliques = 0.5    	#  p__prop_of_links_between_cliques   
+									 		#  not the right name?
+									 		#  is it really the proportion of nodes in one clique 
+									 		#  to link to another clique during one round?
+									 		#  p__prop_of_nodes_in_clique_to_try_to_interlink_in_one_round?
+									 		#  p__prop_to_link_between_two_cliques_in_one_round?
+									 		#  p__prop
+												
+r__density = 0.5
+
+#-----------------------------------------
+
+derive_control_parameters = function (
+n__num_cliques = 3
+alpha__ = 1
+p__prop_of_links_between_cliques = 0.5    	#  p__prop_of_links_between_cliques   
+									 		#  not the right name?
+									 		#  is it really the proportion of nodes in one clique 
+									 		#  to link to another clique during one round?
+									 		#  p__prop_of_nodes_in_clique_to_try_to_interlink_in_one_round?
+									 		#  p__prop_to_link_between_two_cliques_in_one_round?
+									 		#  p__prop
+												
+r__density = 0.5
+)
+{
+	#  Derived control parameters.
+	
+num_nodes_per_clique = n__num_cliques ^ alpha__
+tot_num_nodes = n__num_cliques * num_nodes_per_clique
+
+num_rounds_of_linking_between_cliques = r__density * n__num_cliques * log (n__num_cliques)
+
+target_num_links_between_2_cliques_per_round = 
+								p__prop_of_links_between_cliques * num_nodes_per_clique
+
+num_links_within_one_clique = choose (num_nodes_per_clique, 2)
+tot_num_links_inside_cliques = num_cliques * num_links_within_one_clique
+
+max_possible_num_links_between_cliques = 
+	target_num_links_between_2_cliques_per_round * num_rounds_of_linking_between_cliques
+	
+max_possible_tot_num_links = tot_num_links_inside_cliques + max_possible_num_links_between_cliques
+
+cat ("\n\nInput variable settings")
+cat ("\n\t\t n__num_cliques = ", n__num_cliques)
+cat ("\n\t\t alpha__ = ", alpha__)
+cat ("\n\t\t p__prop_of_links_between_cliques = ", p__prop_of_links_between_cliques)
+cat ("\n\t\t r__density = ", r__density)
+
+cat ("\n\nDerived variable settings")
+cat ("\n\t\t num_nodes_per_clique = ", num_nodes_per_clique)
+cat ("\n\t\t num_rounds_of_linking_between_cliques = ", num_rounds_of_linking_between_cliques)
+cat ("\n\t\t target_num_links_between_2_cliques_per_round = ", target_num_links_between_2_cliques_per_round)
+cat ("\n\t\t num_links_within_one_clique = ", num_links_within_one_clique)
+cat ("\n\t\t tot_num_links_inside_cliques = ", tot_num_links_inside_cliques)
+cat ("\n\t\t max_possible_num_links_between_cliques = ", max_possible_num_links_between_cliques)
+cat ("\n\t\t max_possible_tot_num_links = ", max_possible_tot_num_links)
+cat ("\n\n")
+}
+
+#-----------------------------------------
+
 
 # ```
         #
@@ -448,6 +603,10 @@ gen_spp_PU_amount_table_from_melted_patch_spp_list =
 
 #===============================================================================
 
+    #---------------------------------------------
+    #  Convert the list of lists to a dataframe.
+    #---------------------------------------------
+
 cat ("\n\nBefore melting, subsetsCollAsList = \n")
 print (subsetsCollAsList)
 cat ("\n\n-------------------")
@@ -457,11 +616,28 @@ cat ("\n\nAfter melting, melted_patch_spp_list = \n")
 print (melted_patch_spp_list)
 cat ("\n\n-------------------")
 
+    #-----------------------------------------------
+    #  Convert species and patch lists to vectors.
+    #  Remove duplicates too.
+    #-----------------------------------------------
+
 spp_IDs = unlist (unique (melted_patch_spp_list ["value"]))
 num_spp = length (spp_IDs)
 
+        #---------------------------------------------------------------
+        #  NOTE: the melt() function arbitrarily labelled the patch ID 
+        #  column "L1".
+        #---------------------------------------------------------------
+
 PU_IDs = unlist (unique (melted_patch_spp_list ["L1"]))
 num_PUs = length (PU_IDs)
+
+    #-------------------------------------------------------------
+    #  Strip out just the patch and species information from the 
+    #  melted data and make it into a table.
+    #  Also, marxan requires the table to be sorted in 
+    #  increasing order by planning unit.
+    #-------------------------------------------------------------
 
 spp_PU_amount_table =
     gen_spp_PU_amount_table_from_melted_patch_spp_list (melted_patch_spp_list)
