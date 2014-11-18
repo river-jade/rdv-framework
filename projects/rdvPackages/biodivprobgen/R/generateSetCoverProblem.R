0737c63978fc4e5d168854070925c78e5a100dd9
langfob@gmail.com <langfob@gmail.com@0edbd1c7-074e-7333-919a-b20e77d7077a>
2014-11-18 05:18:10 +0000

    biodivgen: Adding first cut at set cover generator control program.

---
+++ projects/rdvPackages/biodivprobgen/R/generateSetCoverProblem.R
@@ -0,0 +1,198 @@
+#===============================================================================
+
+integerize = function (x) 
+    { 
+    round (x) 
+#    celiing (x)
+#    floor (x)
+    }
+
+add_link = function (node_link_pairs, next_node_link_pair_row, 
+                     smaller_node_ID, larger_node_ID, 
+                     next_link_ID)
+    {
+    cat ("\n\t\t[", smaller_node_ID, ",", larger_node_ID, "]")
+    
+    node_link_pairs [next_node_link_pair_row, "node_ID"] = smaller_node_ID
+    node_link_pairs [next_node_link_pair_row, "link_ID"] = next_link_ID
+            cat ("\n\t\t\tnode_link_pairs [", next_node_link_pair_row, ", ] = \n")
+            print (node_link_pairs [next_node_link_pair_row,])
+    next_node_link_pair_row = next_node_link_pair_row + 1
+    
+    node_link_pairs [next_node_link_pair_row, "node_ID"] = larger_node_ID
+    node_link_pairs [next_node_link_pair_row, "link_ID"] = next_link_ID
+            cat ("\n\t\t\tnode_link_pairs [", next_node_link_pair_row, ", ] = \n")
+            print (node_link_pairs [next_node_link_pair_row,])
+    next_node_link_pair_row = next_node_link_pair_row + 1
+
+    next_link_ID = next_link_ID + 1
+    cat ("\n\t\tnext_link_ID = ", next_link_ID)
+    
+    updated_links = list()
+    updated_links$next_node_link_pair_row = next_node_link_pair_row
+    updated_links$next_link_ID = next_link_ID
+    updated_links$node_link_pairs = node_link_pairs
+    
+    return (updated_links)
+    }
+
+
+
+# derive_control_parameters = 
+#     function (n__num_cliques = 3, 
+#               alpha__ = 1, 
+#               p__prop_of_links_between_cliques = 0.5,     	#  p__prop_of_links_between_cliques   
+#                                                             #  not the right name?
+#                                                             #  is it really the proportion of nodes in one clique 
+#                                                             #  to link to another clique during one round?
+#                                                             #  p__prop_of_nodes_in_clique_to_try_to_interlink_in_one_round?
+#                                                             #  p__prop_to_link_between_two_cliques_in_one_round?
+#                                                             #  p__prop    
+#               r__density = 0.5
+#              )
+#     {
+        
+n__num_cliques                   = 5
+alpha__                          = 0.8
+p__prop_of_links_between_cliques = 0.5  
+r__density                       = 0.5
+        
+        
+    #  Derived control parameters.
+    
+    num_nodes_per_clique = integerize (n__num_cliques ^ alpha__)
+    tot_num_nodes = n__num_cliques * num_nodes_per_clique
+
+    num_independent_set_nodes = n__num_cliques
+    num_dependent_set_nodes = tot_num_nodes - num_independent_set_nodes
+    
+    num_rounds_of_linking_between_cliques = integerize (r__density * n__num_cliques * log (n__num_cliques))
+    
+    target_num_links_between_2_cliques_per_round = 
+        integerize (p__prop_of_links_between_cliques * num_nodes_per_clique)
+    
+    num_links_within_one_clique = choose (num_nodes_per_clique, 2)
+    tot_num_links_inside_cliques = n__num_cliques * num_links_within_one_clique
+    
+    max_possible_num_links_between_cliques = 
+        integerize (target_num_links_between_2_cliques_per_round * num_rounds_of_linking_between_cliques)
+    
+    max_possible_tot_num_links = integerize (tot_num_links_inside_cliques + max_possible_num_links_between_cliques)
+    
+    cat ("\n\nInput variable settings")
+    cat ("\n\t\t n__num_cliques = ", n__num_cliques)
+    cat ("\n\t\t alpha__ = ", alpha__)
+    cat ("\n\t\t p__prop_of_links_between_cliques = ", p__prop_of_links_between_cliques)
+    cat ("\n\t\t r__density = ", r__density)
+    
+    cat ("\n\nDerived variable settings")
+    cat ("\n\t\t num_nodes_per_clique = ", num_nodes_per_clique)
+    cat ("\n\t\t num_rounds_of_linking_between_cliques = ", num_rounds_of_linking_between_cliques)
+    cat ("\n\t\t target_num_links_between_2_cliques_per_round = ", target_num_links_between_2_cliques_per_round)
+    cat ("\n\t\t num_links_within_one_clique = ", num_links_within_one_clique)
+    cat ("\n\t\t tot_num_links_inside_cliques = ", tot_num_links_inside_cliques)
+    cat ("\n\t\t max_possible_num_links_between_cliques = ", max_possible_num_links_between_cliques)
+    cat ("\n\t\t max_possible_tot_num_links = ", max_possible_tot_num_links)
+    cat ("\n\n")
+    
+        #--------------------------------------------------
+        #  Create structures to hold the nodes and links.
+        #--------------------------------------------------    
+    
+    node_IDs = 1:tot_num_nodes
+    
+    clique_IDs = 1 + (0:(tot_num_nodes - 1) %/% num_nodes_per_clique)
+    
+    independent_node_IDs = seq (from=1, 
+                                by=num_nodes_per_clique, 
+                                length.out=n__num_cliques)
+    
+    dependent_set_members = rep (TRUE, tot_num_nodes)
+    dependent_set_members [independent_node_IDs] = FALSE
+    
+    dependent_node_IDs = node_IDs [which (dependent_set_members)]
+    
+#    nodes = cbind (node_IDs, clique_IDs, dependent_set_member)
+    nodes = data.frame (node_ID = node_IDs,
+                        clique_ID = clique_IDs, 
+                        dependent_set_member = dependent_set_members)
+
+    cat ("\n\t\t independent_node_IDs = ", independent_node_IDs)
+    cat ("\n\t\t dependent_node_IDs = ", dependent_node_IDs)
+    
+    cat ("\n\nnodes = \n")
+    print (nodes)
+    cat ("\n\n")
+    
+#    nodes_df = as.data.frame (nodes)
+#    browser()
+    
+    max_possible_tot_num_node_link_pairs = 2 * max_possible_tot_num_links
+    node_link_pairs = matrix (NA, 
+                              nrow=max_possible_tot_num_node_link_pairs, 
+                              ncol=2)
+    node_link_pairs = as.data.frame (node_link_pairs)
+    names (node_link_pairs) = c("node_ID", "link_ID")
+    
+    next_node_link_row = 1
+    next_link_ID = 1
+    cur_clique_ID = 1
+    
+        #  Link all nodes within each clique.
+    all_clique_IDs = 1:n__num_cliques
+
+    cat ("\n\nCliques")
+    
+#df[df$value>3.0,] 
+
+if (num_nodes_per_clique < 2)
+    quit ("\n\n***  num_nodes_per_clique (", num_nodes_per_clique, 
+          ") must be at least 2.\n\n")
+
+
+    num_nodes_per_clique_minus_1 = num_nodes_per_clique - 1
+    next_node_link_pair_row = 1
+    next_link_ID = 1
+
+    for (cur_clique_ID in 1:n__num_cliques)
+        {
+            #  NOTE:  The code in this loop assumes the clique nodes are sorted.  
+            #         These clique nodes are probably already sorted, 
+            #         but this just makes sure, as a safeguard against 
+            #         some future change.
+        cur_clique_nodes_sorted = 
+            sort (nodes [nodes$clique_ID == cur_clique_ID, "node_ID"])
+                cat ("\n\ncur_clique_nodes_sorted for clique ", cur_clique_ID, " = ")
+                print (cur_clique_nodes_sorted)
+        
+        for (cur_idx in 1:num_nodes_per_clique_minus_1)
+            {
+            for (other_node_idx in (cur_idx+1):num_nodes_per_clique)
+                {
+                updated_links = 
+                    add_link (node_link_pairs, next_node_link_pair_row, 
+                              cur_clique_nodes_sorted [cur_idx], 
+                              cur_clique_nodes_sorted [other_node_idx], 
+                              next_link_ID)
+                next_node_link_pair_row = updated_links$next_node_link_pair_row
+                next_link_ID = updated_links$next_link_ID
+                node_link_pairs = updated_links$node_link_pairs
+                }
+            }
+        }
+        
+    cat ("\n\nnode_link_pairs = \n")
+    print (node_link_pairs)
+    cat ("\n\n")
+
+#===============================================================================
+
+# derive_control_parameters (n__num_cliques                   = 5, 
+#                            alpha__                          = 0.8, 
+#                            p__prop_of_links_between_cliques = 0.5,  
+#                            r__density                       = 0.5
+#                           )
+        
+#===============================================================================
+
+
