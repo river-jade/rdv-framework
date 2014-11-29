8863f0d23955ebdd04e8417a2f5f5cf55929b40b
langfob@gmail.com <langfob@gmail.com@0edbd1c7-074e-7333-919a-b20e77d7077a>
2014-11-29 07:49:21 +0000

    biodivprobgen: Added code to do tzar emulation.

---
+++ projects/rdvPackages/biodivprobgen/R/emulatingTzarFlag.R
@@ -0,0 +1,22 @@
+#===============================================================================
+
+                #  source ("emulatingTzarFlag.R")
+
+#  Control flag to tell whether your code should be run under the tzar 
+#  emulator or under normal tzar.  
+
+#  This file only exists because more than one R file may need to know 
+#  the value of this flag simultaneously and having its value sourced 
+#  through this file makes sure that they all agree on the value.
+
+#  History
+
+#  BTL - 2014 11 28 - Created.
+
+#===============================================================================
+
+    #  Comment out whichever line is no longer appropriate.
+#emulateRunningUnderTzar = TRUE
+emulateRunningUnderTzar = FALSE
+
+#===============================================================================
--- projects/rdvPackages/biodivprobgen/R/model.R
+++ projects/rdvPackages/biodivprobgen/R/model.R
@@ -1,57 +1,27 @@
 #===============================================================================
 
-#                               model.R
-
-#  source ('model.R')
-
-#===============================================================================
-
-#  History:
-
-#  2014 11 23 - BTL - cloned from the g2 version of this same file.
+                            #-----------------------------------#
+                            #               model.R             #
+                            #                 for               #
+                            #  example-tzar-emulator-R project  #
+                            #-----------------------------------#
 
 #===============================================================================
 
-cat ("\n\nCurrent directory is: '", getwd(), "'\n\n")
+source ("emulatingTzarFlag.R")
 
-cat ("\n\nJust before sourcing generateSetCoverProblem.R, parameters = \n")
-print (parameters)
-cat ("\n\n")
+tzarEmulation_scratchFileName = "~/D/rdv-framework/projects/rdvPackages/biodivprobgen/R/tzarEmulation_scratchFile.txt"
 
-    #----------------------------------------------------------------
-    #  Need to see whether emulating tzar run or really doing one.
-    #
-    #  First, need to get emulateRunningUnderTzar flag value that
-    #  indicates whether to emulate or not.  Also get name of file
-    #  where tzar output directory will be written if emulating,
-    #  i.e., tzarEmulation_scratchFileName.
-    #----------------------------------------------------------------
-
-#source ('emulateRunningUnderTzar.R')
-emulateRunningUnderTzar = FALSE
-
-    #---------------------------------------------------------
-    #  If doing a normal tzar run, then start the model now.
-    #---------------------------------------------------------
-
-if (! emulateRunningUnderTzar)
+if (! emulateRunningUnderTzar)  
     {
-    source ("generateSetCoverProblem.R")
-
-    } else
-    {
-        #-------------------------------------------------------
-        #  Emulating rather than doing a normal tzar run.
-        #  Capture the tzar output directory name and write it
-        #  to a file in the project directory that can be read
-        #  after tzar exits.
-        #-------------------------------------------------------
-
-    cat (parameters$fullTzarExpOutputDirRootWithSlash,
-         "\n",
-         file = tzarEmulation_scratchFileName,
-         sep='' )
+    source ("generateSetCoverProblem.R")    #  not emulating, running regular tzar
     
+    } else    #  emulating
+    {
+    parameters$tzarEmulation_scratchFileName = tzarEmulation_scratchFileName
+    
+    cat (parameters$fullOutputDirWithSlash, "\n",
+       file = tzarEmulation_scratchFileName, sep='' )
     }
 
 #===============================================================================
