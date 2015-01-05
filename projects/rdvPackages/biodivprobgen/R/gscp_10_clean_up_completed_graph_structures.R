4b6ad888fe37a5d323d4984a7cb9085b6a750c17
langfob@gmail.com <langfob@gmail.com@0edbd1c7-074e-7333-919a-b20e77d7077a>
2015-01-05 05:22:57 +0000

    biodivprobgen: Removed all traces of add_link().

--- projects/rdvPackages/biodivprobgen/R/gscp_10_clean_up_completed_graph_structures.R
+++ projects/rdvPackages/biodivprobgen/R/gscp_10_clean_up_completed_graph_structures.R
@@ -70,22 +70,6 @@ node_link_pairs = matrix (NA,
 node_link_pairs = as.data.frame (node_link_pairs)
 names (node_link_pairs) = c("node_ID", "link_ID")
 
-# next_node_link_pair_row = 1
-# 
-# for (cur_link_ID in 1:num_unique_edge_list)
-#     {
-#     updated_links = 
-#         add_link (node_link_pairs, 
-#                   next_node_link_pair_row, 
-#                   unique_edge_list [cur_link_ID, 1], 
-#                   unique_edge_list [cur_link_ID, 2], 
-#                   cur_link_ID)
-#     
-#     node_link_pairs = updated_links$node_link_pairs
-#     
-#     next_node_link_pair_row = updated_links$next_node_link_pair_row
-#     }
-
 next_PU_spp_pair_row = 1
 
 for (cur_spp_ID in 1:num_unique_edge_list)
--- projects/rdvPackages/biodivprobgen/R/gscp_4_support_functions.R
+++ projects/rdvPackages/biodivprobgen/R/gscp_4_support_functions.R
@@ -39,48 +39,6 @@ integerize = switch (integerize_string,
 
 #-------------------------------------------------------------------------------
 
-add_link = function (node_link_pairs, next_node_link_pair_row, 
-                     smaller_node_ID, larger_node_ID, 
-                     next_link_ID)
-    {
-        #  NOTE:  I think that this the ordering of the node IDs within 
-        #           each pair is important later on.  That is, when  
-        #           trying to identify duplicated links, the unique()  
-        #           call that is used will only work if the pairs are 
-        #           ordered within pair, i.e., if all from nodes 
-        #           have a value less than or equal to the to value.
-        #           That wouldn't be necessary if these were directed links, 
-        #           but undirected, you couldn't recognize duplicates if 
-        #           the order was allowed to occur both ways, i.e., (3,5) and 
-        #           (5,3) would not be flagged as being duplicates.
-
-#     cat ("\n\t\t[", smaller_node_ID, ",", larger_node_ID, "]")
-    
-    node_link_pairs [next_node_link_pair_row, "node_ID"] = smaller_node_ID
-    node_link_pairs [next_node_link_pair_row, "link_ID"] = next_link_ID
-#              cat ("\n\t\t\tnode_link_pairs [", next_node_link_pair_row, ", ] = \n")
-#              print (node_link_pairs [next_node_link_pair_row,])
-
-    next_node_link_pair_row = next_node_link_pair_row + 1    
-    node_link_pairs [next_node_link_pair_row, "node_ID"] = larger_node_ID
-    node_link_pairs [next_node_link_pair_row, "link_ID"] = next_link_ID
-#              cat ("\n\t\t\tnode_link_pairs [", next_node_link_pair_row, ", ] = \n")
-#              print (node_link_pairs [next_node_link_pair_row,])
-    next_node_link_pair_row = next_node_link_pair_row + 1
-
-#     next_link_ID = next_link_ID + 1
-#              cat ("\n\t\tnext_link_ID = ", next_link_ID)
-    
-    updated_links = list()
-    updated_links$next_node_link_pair_row = next_node_link_pair_row
-#     updated_links$next_link_ID = next_link_ID
-    updated_links$node_link_pairs = node_link_pairs
-    
-    return (updated_links)
-    }
-
-#-------------------------------------------------------------------------------
-
     #  Run marxan.
         #  Should include this in the marxan package.
 
