77c6baf1561995d1c4108ed2e950cde658bcfd0a
langfob@gmail.com <langfob@gmail.com@0edbd1c7-074e-7333-919a-b20e77d7077a>
2015-01-07 04:14:14 +0000

    biodivprobgen: Added code for generating and measuring global attributes of spp/PU bipartite networks using bipartite package and graph package.  Seems to be running correctly.

---
+++ projects/rdvPackages/biodivprobgen/R/gscp_11a_network_measures_using_bipartite_package.R
@@ -0,0 +1,127 @@
+#===============================================================================
+
+            #  gscp_11a_network_measures_using_bipartite_package.R
+
+#===============================================================================
+
+                    #  Code for using bipartite package.
+
+#===============================================================================
+
+library (bipartite)
+
+###  Build input matrix for bipartite package...
+
+bpm = matrix (0, 
+              #rep (0, num_spp*num_PUs), 
+              nrow=num_spp, 
+              ncol=num_PUs, 
+              byrow=TRUE
+                #  Not sure whether to have dimnames or not.  
+                #  Doesn't seem to hurt anything at the moment...
+              , 
+              dimnames=list (spp_vertex_names, 
+                             PU_vertex_names)
+              )
+
+for (edge_idx in 1:num_PU_spp_pairs)
+    {
+#     cur_row = PU_spp_pair_indices [edge_idx, "spp_ID"]
+#     cur_col = PU_spp_pair_indices [edge_idx, "PU_ID"]
+    cur_row = PU_spp_pair_indices [edge_idx, spp_col_name]
+    cur_col = PU_spp_pair_indices [edge_idx, PU_col_name]
+#    cat ("\ncur rc = [", cur_row, ", ", cur_col, "]")
+
+        #  I'm making this be "1 + ..." instead of just "1", 
+        #  in case at some point, I need to keep counts of 
+        #  duplicates instead of just presence/absence.  
+        #  With unique values, you get the same matrix 
+        #  either way.
+    bpm [cur_row, cur_col] = 1 + bpm [cur_row, cur_col]
+    }
+
+if (DEBUG_LEVEL > 0)
+    {
+    cat ("\n\nbpm = \n")
+    print (bpm)
+    cat ("\n\n")
+    }
+
+all_except_slow_indices <- 
+    c(
+        "number of species", 
+        "connectance", 
+        "web asymmetry", 
+        "links per species", 
+#           "number of compartments",           #  #6a slowest ("calls a recursive and hence relatively slow algorithm.") 
+#           "compartment diversity",            #  #6b slowest ("calls a recursive and hence relatively slow algorithm.")
+        "cluster coefficient", 
+#            "degree distribution",              #  #3 slowest ("somewhat time consuming")
+        "mean number of shared partners", 
+        "togetherness", 
+        "C score", 
+        "V ratio", 
+#            "discrepancy",                      #  #7c slowest ("excluding discrepancy can also moderately decrease computation time")
+#            "nestedness",                       #  #5a slowest ("not the fastest of routines")
+#            "weighted nestedness",              #  #5b slowest ("not the fastest of routines")
+        "ISA", 
+        "SA", 
+#            "extinction slope",                 #  #1a slowest
+#            "robustness",                       #  #1b slowest 
+        "niche overlap", 
+#            "weighted cluster coefficient",     #  #2 slowest
+        "weighted NODF", 
+        "partner diversity", 
+        "generality", 
+        "vulnerability", 
+        "linkage density", 
+        "weighted connectance", 
+#            "Fisher alpha",                     #  #4 slowest ("computed iteratively and hence time consuming")
+        "interaction evenness", 
+        "Alatalo interaction evenness", 
+        "Shannon diversity", 
+        "functional complementarity" 
+)
+#            , "H2                                 #  #7a slowest ("require an iterative, heuristic search algorithm", #7b - same for specialisation asymmetry but it's not in this list)
+#        )
+
+
+    bipartite_metrics_from_bipartite_package <- 
+#         t (networklevel (bpm, index=c ("links per species",
+#                                      "H2",
+#                                      "interaction evenness")))
+        t (networklevel (bpm, index=all_except_slow_indices))
+
+cat ("\n\nbipartite_metrics_from_bipartite_package = \n")
+print (bipartite_metrics_from_bipartite_package)
+
+    #  Clean up metric names...
+    #  
+    #  The bipartite metrics output has spaces in some of the metric names. 
+    #  This can cause problems sometimes for downstream uses of the output, so 
+    #  you need to replace the spaces with something other than white space.  
+    #  Here, I'll replace them with underscores.  Some of the metrics already 
+    #  use periods instead of spaces, but I won't mess with those for now in 
+    #  case it helps match them up with some other expectation from a paper or 
+    #  something.
+
+library (stringr)
+
+metrics_col_names = colnames (bipartite_metrics_from_bipartite_package)  
+cat ("\n\nmetrics_col_names = \n")
+print (metrics_col_names)
+
+new_metrics_col_names = 
+    str_replace_all (string=metrics_col_names, pattern=" ", repl="_")
+cat ("\n\nnew_metrics_col_names = \n")
+print (new_metrics_col_names)
+cat ("\n\n")
+
+colnames (bipartite_metrics_from_bipartite_package) = new_metrics_col_names
+
+cat ("\n\nbipartite_metrics_from_bipartite_package with spaces removed from column names = \n")
+print (bipartite_metrics_from_bipartite_package)
+cat ("\n\n")
+
+#===============================================================================
+
---
+++ projects/rdvPackages/biodivprobgen/R/gscp_11b_network_measures_using_igraph_package.R
@@ -0,0 +1,261 @@
+#===============================================================================
+
+            #  gscp_11b_network_measures_using_igraph_package.R
+
+#===============================================================================
+
+                    #  Code for using igraph package.
+
+#===============================================================================
+
+#browser()
+
+#if (FALSE)
+#{
+library (igraph)
+
+vertices = data.frame (name=c(spp_vertex_names, PU_vertex_names), 
+                       type=c(rep(FALSE, num_spp),
+                              rep(TRUE, num_PUs)))
+
+    #  Does igraph require me to rename the columns to say 
+    #  "from" and "to instead of "PU_ID" and "spp_ID"?
+# bg = graph.data.frame (edge_df, directed=FALSE, vertices=vertices)
+
+cat ("\n\nJust before graph.data.frame()\n")
+
+cat ("\nnames (PU_spp_pair_indices = \n")
+print (names (PU_spp_pair_indices))
+
+cat ("\nnames (PU_spp_pair_names = \n")
+print (names (PU_spp_pair_names))
+
+#names (PU_spp_pair_indices) = c("from", "to")
+#cat ("\n\nAfter changing names\n")
+#cat ("\nnames (PU_spp_pair_indices = \n")
+#print (names (PU_spp_pair_indices))
+
+#bg = graph.data.frame (PU_spp_pair_indices, directed=FALSE, vertices=vertices)
+bg = graph.data.frame (PU_spp_pair_names, directed=FALSE, vertices=vertices)
+
+#names (PU_spp_pair_indices) = c(PU_col_name, spp_col_name)
+#cat ("\n\nAfter reinstating old names\n")
+#cat ("\nnames (PU_spp_pair_indices = \n")
+#print (names (PU_spp_pair_indices))
+#}
+
+#===============================================================================
+
+cat ("\n\n=====>  Under igraph, is.bipartite (bg) = ", is.bipartite (bg), "\n")
+#Echo results...
+
+
+print(bg)
+
+print (V(bg))
+print (V(bg)$type)
+print (E(bg))
+
+plot(bg)
+
+bgp = bipartite.projection(bg)
+plot(bgp$proj1)
+plot(bgp$proj2)
+
+### Compute the bipartite measures from the Latapy et al 2008 paper.
+
+# These are extracts from point 6 of the R Recipes page of the igraph wiki:
+# 
+# http://igraph.wikidot.com/r-recipes
+# 
+# **Reference: Matthieu Latapy, Clemence Magnien, Nathalie Del Vecchio, Basic notions for the analysis of large two-mode networks, Social Networks 30 (2008) 31–48**
+# 
+# NOTE:  The code doesn't work as it is given on the web page.  For example, it 
+# starts off by referencing a graph g that has not been defined.  Later, it 
+# starts in on network transitivity and references "target", which has not been 
+# defined either.  Finally, it references several functions before they're 
+# defined, e.g., ccBip().  In each case, I've done a little hack to get it to 
+# work here.
+
+    # Number of top and bottom nodes
+top<-length(V(bg)[type==FALSE])
+bottom<-length(V(bg)[type==TRUE])
+
+    # Number of edges
+m<-ecount(bg)
+
+    # Mean degree for top and bottom nodes
+ktop<-m/top
+kbottom<-m/bottom
+
+    # Density for bipartite network
+bidens<-m/(top*bottom)
+
+    # Largest connected component for top and bottom nodes:
+gclust<-clusters(bg, mode='weak')
+lcc<-induced.subgraph(bg, V(bg)[gclust$membership==1])
+lcctop<-length(V(lcc)[type==FALSE])
+lccbottom<-length(V(lcc)[type==TRUE])
+
+    # Mean distance for top and bottom nodes
+distop<-mean(shortest.paths(lcc, v=V(lcc)[type==FALSE], to=V(lcc)[type==FALSE], mode = 'all'))
+disbottom<-mean(shortest.paths(lcc, v=V(lcc)[type==TRUE], to=V(lcc)[type==TRUE], mode = 'all'))
+
+#######################################################
+
+    #  BTL additions to get it to work...
+target = bg
+
+    # Network transitivity
+trtarget<-graph.motifs(target, 4)
+ccglobal<-(2*trtarget[20])/trtarget[14]
+
+    # The last clustering measures relies on the functions ccBip 
+    # wrote by Gabor Csardi, while ccLowDot and ccTopDot are 
+    # essentially the same function with only a minor change
+ccBip <- function(bg) 
+    {
+    if (! "name" %in% list.vertex.attributes (bg)) 
+        {
+        V(bg)$name <- seq_len (vcount(bg))
+        }
+    neib <- get.adjlist (bg)
+    names (neib) <- V(bg)$name
+    proj <- bipartite.projection(bg)
+    
+    lapply (proj, 
+            function(x) 
+                {
+                el <- get.edgelist(x)
+                sapply (V(x)$name, 
+                        function (v) 
+                            {
+                            subs <- el[,1]==v | el[,2]==v
+                            
+                            f <- function (un, vn) length (union (un, vn))
+                            
+                            vals <- E(x)[subs]$weight / 
+                                    unlist (mapply (f, 
+                                                    neib[el[subs,1]],
+                                                    neib[el[subs,2]]))
+                            mean (vals)
+                            }
+                        )
+                }
+            )
+    }
+
+ccLowDot <- function(bg) {
+if (! "name" %in% list.vertex.attributes(bg)) {
+  V(bg)$name <- seq_len(vcount(bg))
+}
+neib <- get.adjlist(bg)
+names(neib) <- V(bg)$name
+proj <- bipartite.projection(bg)
+lapply(proj, function(x) {
+  el <- get.edgelist(x)
+  sapply(V(x)$name, function(v) {
+    subs <- el[,1]==v | el[,2]==v
+    f <- function(un, vn) min(length(un), length(vn))
+    vals <- E(x)[subs]$weight /
+      unlist(mapply(f, neib[el[subs,1]], neib[el[subs,2]]))
+    mean(vals)
+  })
+})
+}
+
+ccTopDot <- function(bg) {
+if (! "name" %in% list.vertex.attributes(bg)) {
+  V(bg)$name <- seq_len(vcount(bg))
+}
+neib <- get.adjlist(bg)
+names(neib) <- V(bg)$name
+proj <- bipartite.projection(bg)
+lapply(proj, function(x) {
+  el <- get.edgelist(x)
+  sapply(V(x)$name, function(v) {
+    subs <- el[,1]==v | el[,2]==v
+    f <- function(un, vn) max(length(un), length(vn))
+    vals <- E(x)[subs]$weight /
+      unlist(mapply(f, neib[el[subs,1]], neib[el[subs,2]]))
+    mean(vals)
+  })
+})
+}
+
+    #  BTL - Had to move this code down below the definitions of the functions 
+    #        used here (ccBip(), etc.).
+    
+    # Clustering coefficients (cc, cclowdot, cctopdot) for top and bottom nodes
+ccPointTarg<-ccBip(bg)
+cctop<-mean(ccPointTarg$proj1, na.rm=TRUE)
+ccbottom<-mean(ccPointTarg$proj2, na.rm=TRUE)
+ccLowTarg<-ccLowDot(bg)
+cclowdottop<-mean(ccLowTarg$proj1, na.rm=TRUE)
+cclowdotbottom<-mean(ccLowTarg$proj2, na.rm=TRUE)
+ccTopTarg<-ccTopDot(bg)
+cctopdottop<-mean(ccTopTarg$proj1, na.rm=TRUE)
+cctopdobottom<-mean(ccTopTarg$proj2, na.rm=TRUE)
+
+redundancy<-function(g
+                        #  Added by BTL since this only seemed to do bottom.
+                     , top_bottom_vertex_type=FALSE
+                     )  
+    {
+        redundancy<-c()
+
+                       #  Added by BTL since this only seemed to do bottom.
+        for(i in V(g)[which(V(g)$type==top_bottom_vertex_type)]){
+#        for(i in V(g)[which(V(g)$type==FALSE)]){
+            
+                overlap <- 0
+                nei<-neighbors(g,i)
+                
+                    #  Correction suggested by Tamas Nepusz
+                #if(length(nei)>0){
+                if(length(nei)>1){
+                    
+                        comb<-combn(nei, 2)     
+                        for(c in seq(1:dim(comb)[2])){
+                                unei<-neighbors(g,comb[1,c])
+                                wnei<-neighbors(g,comb[2,c])
+
+                                    #  Correction suggested by Tamas Nepusz
+                                #redund<-Reduce(union, list(unei,wnei))
+                                #redund<-Reduce(setdiff, list(redund,i))
+                                redund <- setdiff(intersect(unei, wnei), i)
+
+                                if(length(redund)>0){
+                                        overlap <- overlap + 1
+                                }
+                        }
+                }
+                if(overlap > 0){
+                        n <- length(nei)
+                        norm<-2.0/(n*(n-1))
+                } else {
+                        norm <- 1
+                }
+                redundancy<-append(redundancy, overlap*norm)
+        }
+        return(redundancy)
+}
+
+bottom_bg_redundancy = redundancy (bg, FALSE)
+cat ("\n\nbottom bg_redundancy = \n")
+print (bottom_bg_redundancy)
+cat ("\nmean bottom_bg_redundancy = ", mean (bottom_bg_redundancy))
+cat ("\nmedian bottom_bg_redundancy = ", median (bottom_bg_redundancy))
+
+top_bg_redundancy = redundancy (bg, TRUE)
+cat ("\n\ntop bg_redundancy = \n")
+print (top_bg_redundancy)
+cat ("\nmean top_bg_redundancy = ", mean (top_bg_redundancy))
+cat ("\nmedian top_bg_redundancy = ", median (top_bg_redundancy))
+
+cat ("\n")
+
+browser()
+
+#===============================================================================
+
