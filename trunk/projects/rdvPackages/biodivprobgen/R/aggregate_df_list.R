1be7bdd603bad4592bb3a80e4de33525eff51f7f
langfob@gmail.com <langfob@gmail.com@0edbd1c7-074e-7333-919a-b20e77d7077a>
2015-01-10 04:43:30 +0000

    biodivprobgen: Added some recent failed result lists to be incorporated.  Am about to make lots of changes to this code so that it can be run more repeatedly as a function, etc.

--- trunk/projects/rdvPackages/biodivprobgen/R/aggregate_df_list.R
+++ trunk/projects/rdvPackages/biodivprobgen/R/aggregate_df_list.R
@@ -3,6 +3,30 @@
 #  http://rpubs.com/justmarkham/dplyr-tutorial
 
 
+JUST FINISHED - 2015 01 09 11:30 PM
+full_run_list__test100unifRand_p_r_n_a_seed_301_with_error_trapping_and_timing = 1:100
+failed_run_list__test100unifRand_p_r_n_a_seed_301_with_error_trapping_and_timing = 
+    c (2, 3, 5, 9, 10, 11, 12, 13, 15, 19, 21, 23, 26, 28, 30, 
+       31, 33, 36, 37, 41, 42, 44, 46, 48, 51, 52, 54, 55, 56, 
+       57, 59, 60, 62, 63, 64, 65, 69, 70, 73, 74, 75, 76, 77, 
+       78, 83, 86, 87, 95, 96, 98, 99) 
+
+
+full_run_list__test100unifRand_p_r_n_a_seed_401_with_error_trapping_and_timing = 1:100
+failed_run_list__test100unifRand_p_r_n_a_seed_401_with_error_trapping_and_timing = 
+Failed IDs were: [1, 2, 4, 7, 8, 12, 14, 20, 22, 25, 29, 31, 32, 37, 39, 40, 41, 43, 45, 46, 47, 50, 52, 53, 55, 59, 62, 64, 65, 71, 72, 77, 82, 83, 84, 86, 88, 91, 94, 95, 96, 98, 100] 
+
+
+
+full_run_list__test100unifRand_p_r_n_a_seed_501_with_error_trapping_and_timing = 1:100
+failed_run_list__test100unifRand_p_r_n_a_seed_501_with_error_trapping_and_timing = 
+Failed IDs were: [2, 4, 6, 8, 13, 19, 22, 23, 24, 25, 28, 29, 33, 37, 39, 40, 41, 42, 43, 45, 49, 50, 53, 54, 55, 59, 67, 68, 71, 72, 75, 76, 78, 79, 81, 88, 89, 91, 92, 93, 98, 99] 
+
+
+full_run_list__test100unifRand_p_r_n_a_seed_601_with_error_trapping_and_timing = 1:100
+failed_run_list__test100unifRand_p_r_n_a_seed_601_with_error_trapping_and_timing = 
+Failed IDs were: [3, 4, 6, 7, 8, 10, 11, 13, 16, 17, 19, 26, 27, 28, 30, 31, 33, 34, 41, 42, 43, 44, 46, 47, 48, 49, 52, 57, 59, 61, 65, 68, 79, 87, 90, 93, 94, 95, 97, 98, 99] 
+
 
 full_run_list = 1:327
 failed_run_list = c (4,6,7,9,12,14,33,34,37,41,45,46,47,
@@ -12,6 +36,9 @@ failed_run_list = c (4,6,7,9,12,14,33,34,37,41,45,46,47,
                     201,202,203,205,206,207,214,215,219,220,226,234,245,249,
                     258,267,268,277,279,282,288,290,291,292,298,
                     303,304,307,312,313,314,321)
+
+full_run_list = 1:100
+failed_run_list = failed_run_list__test100unifRand_p_r_n_a_seed_301_with_error_trapping_and_timing
 
 successful_run_list = full_run_list
 for (kkk in failed_run_list)
